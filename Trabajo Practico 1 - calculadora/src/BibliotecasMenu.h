2b03a6243cd5b5880e363bdaaa3567870557c8a8
Lucas javier Altamirano <altamiranolucasjavier@gmail.com>
2021-04-15 23:55:04 -0300

    trabajo finalizado

---
+++ Trabajo Practico 1 - calculadora/src/BibliotecasMenu.h
@@ -0,0 +1,17 @@
+/*
+ * BibliotecasMenu.h
+ *
+ *  Created on: 14 abr. 2021
+ *      Author: Lucas
+ */
+
+
+
+#ifndef BIBLIOTECASMENU_H_
+#define BIBLIOTECASMENU_H_
+/*
+*	@brief menu muestra 5 opciones
+*/
+void Menu(); //Prototipo
+
+#endif /* BIBLIOTECASMENU_H_ */
